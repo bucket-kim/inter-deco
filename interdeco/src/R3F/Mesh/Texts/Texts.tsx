b7176b50b7a3f5ca56de0b31b275d9bde1417175
Kyoung Hoon Kim <brian@groovejones.com>
2024-07-01 15:51:45 -0400

    title changed

--- interdeco/src/R3F/Mesh/Texts/Texts.tsx
+++ interdeco/src/R3F/Mesh/Texts/Texts.tsx
@@ -1,23 +1,23 @@
 import { Center, Text3D } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { Group } from "three";
 
 const Texts = () => {
   const { width: w } = useThree((state) => state.size);
   const groupRef = useRef<Group>(null);
 
-  useEffect(() => {
-    if (!groupRef.current) return;
-    if (w < 440) {
-      groupRef.current.scale.set(0.3, 0.3, 0.3);
-    } else {
-      groupRef.current.scale.set(0.5, 0.5, 0.5);
-    }
-  }, [w]);
+  // useEffect(() => {
+  //   if (!groupRef.current) return;
+  //   if (w < 440) {
+  //     groupRef.current.scale.set(0.3, 0.3, 0.3);
+  //   } else {
+  //     groupRef.current.scale.set(0.5, 0.5, 0.2);
+  //   }
+  // }, [w]);
 
   return (
-    <group position={[0, 2.8, 0]} scale={0.5} ref={groupRef}>
+    <group position={[0, 2.8, 0]} scale={[0.5, 0.5, 0.2]} ref={groupRef}>
       <Center>
         <Text3D
           font={"/Inter_Bold.json"}
@@ -27,10 +27,10 @@ const Texts = () => {
           bevelThickness={0.1}
           height={0.5}
           lineHeight={0.5}
-          letterSpacing={-0.06}
+          letterSpacing={0.0}
         >
-          BLOG RECORDs
-          <meshStandardMaterial color={"#ffffff"} toneMapped={true} />
+          BLOGs
+          <meshStandardMaterial color={"#ffffff"} toneMapped={false} />
         </Text3D>
       </Center>
     </group>
